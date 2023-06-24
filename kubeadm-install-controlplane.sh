0efe339c1deb7730506ea46ec7b5f8bd0de35e5d
Kev <ksayad@gmail.com>
2023-06-24 15:15:38 -0400

    file per node type

--- kubeadm-install-controlplane.sh
+++ kubeadm-install-controlplane.sh
@@ -39,9 +39,24 @@ sudo swapoff -a
 # Remove the swap permanently
 sudo sed -e '/swap/ s/^#*/#/' -i $FSTAB_FILE
 
-# Install kubeadm
+# Install containerd (from the Docker documentation: https://docs.docker.com/engine/install/ubuntu/)
 
-# Install containerd
+sudo apt-get update && sudo apt-get install -y \
+    ca-certificates \
+    curl gnupg
+
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update && sudo apt-get install -y containerd.io
+
+# Install kubeadm
 
 # Install CNI flannel
 
