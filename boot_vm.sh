a7d087f0f5eeade876f17d568cd76397800ff918
Kev <ksayad@gmail.com>
2023-07-12 00:01:25 -0400

    consolidated boot up and down scripts

--- boot_vm.sh
+++ boot_vm.sh
@@ -1,4 +1,16 @@
 #!/usr/bin/bash
 
-VBoxManage startvm "kube-controlplane" --type headless
-VBoxManage startvm "kube-worker1" --type headless
+VM_CONTROL_PLANE="kube-controlplane"
+VM_WORKER_NODE="kube-worker1"
+
+if [ "$1" == "up" ]; then
+    echo "Starting the two nodes in headless mode..."
+    VBoxManage startvm $VM_CONTROL_PLANE --type headless
+    VBoxManage startvm $VM_WORKER_NODE --type headless
+elif [ "$1" == "halt" ]; then
+    echo "Shutting down the two nodes (acpi power button)"
+    VBoxManage controlvm $VM_CONTROL_PLANE acpipowerbutton
+    VBoxManage controlvm $VM_WORKER_NODE acpipowerbutton
+else
+    echo "Expected one argument. up to start the VMs. halt to stop the VMs (ACPI Power button). Example: ./boot_vm.sh up to start the VMs"
+fi
--- shutdown_vm.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/bash
-
-VBoxManage controlvm "kube-controlplane" acpipowerbutton
-VBoxManage controlvm "kube-worker1" acpipowerbutton
