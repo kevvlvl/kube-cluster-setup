0375437907547dcc7664038b9232eae2a7739fbd
Kev <ksayad@gmail.com>
2023-07-13 22:54:04 -0400

    rename and enhance cluster script

--- vbox-cluster.sh
+++ vbox-cluster.sh
@@ -11,6 +11,12 @@ elif [ "$1" == "halt" ]; then
     echo "Shutting down the two nodes (acpi power button)"
     VBoxManage controlvm $VM_CONTROL_PLANE acpipowerbutton
     VBoxManage controlvm $VM_WORKER_NODE acpipowerbutton
+elif [ "$1" == "info" ]; then
+    echo "List of VMs:"
+    VBoxManage list vms
+    echo "---------------------"
+    echo "List of running VMs:"
+    VBoxManage list runningvms
 else
-    echo "Expected one argument. up to start the VMs. halt to stop the VMs (ACPI Power button). Example: ./boot_vm.sh up to start the VMs"
+    echo "Expected one argument. up to start the VMs. halt to stop the VMs (ACPI Power button) or info for VM status. Example: ./vbox-cluster.sh up to start the VMs"
 fi
