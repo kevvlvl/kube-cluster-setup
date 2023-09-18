c15db923df029eca297b76effa9200627a12fd35
Kev <ksayad@gmail.com>
2023-09-18 17:01:54 -0400

    helper cli

--- cluster.sh
+++ cluster.sh
@@ -2,11 +2,11 @@
 
 source kubeadm-vars.sh
 
-if [ "$1" == "start" ]; then
+if [ "$1" == "up" ]; then
     echo "Starting the two nodes"
     virsh start $CONTROL_PLANE_NAME
     virsh start $WORKER_NAME
-elif [ "$1" == "stop" ]; then
+elif [ "$1" == "halt" ]; then
     echo "Shutting down the two nodes"
     virsh shutdown $CONTROL_PLANE_NAME
     virsh shutdown $WORKER_NAME
@@ -15,7 +15,7 @@ elif [ "$1" == "info" ]; then
     virsh list --all
 else
     echo "Expected one argument:"
-    echo "cluster.sh start  Start the kvm VMs"
-    echo "cluster.sh stop   Stop the kvm VMs using ACPI Powerbutton Shutdown"
-    echo "cluster.sh info   Status of kvm VMs"
+    echo "cluster.sh up     Start the VMs"
+    echo "cluster.sh halt   Shutdown the VMs"
+    echo "cluster.sh info   Status info of VMs"
 fi
