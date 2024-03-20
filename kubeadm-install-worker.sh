ec41fe9fdd467c951ab29ff23398996e49e7f0c0
Kev <ksayad@gmail.com>
2024-03-19 21:46:30 -0400

    update k8s repo in install scripts

--- kubeadm-install-worker.sh
+++ kubeadm-install-worker.sh
@@ -120,9 +120,9 @@ EOF
         ca-certificates \
         curl
 
-    curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-archive-keyring.gpg
+    curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 
-    echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+    echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.28/deb/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
     sudo apt-get update && sudo apt-get install -y \
         kubelet \
